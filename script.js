c94e8eac2939f6aacbd0ccb55c17d30bf8f82e9c
narubeast <mohmahn@gmail.com>
2017-03-03 20:49:09 +0530

    .

---
+++ script.js
@@ -0,0 +1,12 @@
+var res = 16;
+
+$(document).ready(function(){
+  for(i=0; i<256; i++) {
+    $('#grid').append("<div class='lolk'></div>");
+  }
+  /*makeGrid();*/
+});
+
+function makeGrid() {
+
+};
