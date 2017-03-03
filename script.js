070f916843eae6258718241c3da85b7ef0839352
narubeast <mohmahn@gmail.com>
2017-03-03 20:54:36 +0530

    v1.0

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
 var res = 16;
 
 $(document).ready(function(){
-  for(i=0; i<256; i++) {
-    $('#grid').append("<div class='lolk'></div>");
-  }
-  /*makeGrid();*/
+  makeGrid();
 });
 
 function makeGrid() {
-
+  for(i=0; i<256; i++) {
+    $('#grid').append("<div class='lolk'></div>");
+  }
+  plot();
 };
+
+function plot(){
+  $('div').mouseenter(function(){
+    $(this).addClass('a');
+  });
+}
